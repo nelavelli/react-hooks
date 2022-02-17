42526119370288087024015cc1d59d05926f56c6
Naresh <nelavelli.c@gmail.com>
2022-02-17 20:31:58 +0800

    eslint added wit prettier dependencies

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This project will have implementation for all react hooks.
 
 npm init -y # initialize the package manger by creating package.json.
 npm install react react-dom next # installs react and dependencies.
+
+npm install eslint eslint-plugin-react eslint-plugin-react-hooks prettier --save-dev
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const InputElement = () => {
-  const [inputText, setInputText] = useState("");
-  const [historyList, setHistoryList] = useState([]);
+  const random_boolean = Math.random() > 0.5;
+  const [isLoading, setIsLoading] = useState(random_boolean);
 
-  return (
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return isLoading ? (
+    <span> Loading... </span>
+  ) : (
     <div>
-      <input
-        onChange={(e) => {
-          setInputText(e.target.value);
-          setHistoryList([...historyList, e.target.value]);
-        }}
-        type="text"
-        placeholder="Enter Some Text"
-      />
-      <br />
-      {inputText}
-      <hr />
-      <br />
-      {historyList.map((rec) => {
-        return <div> {rec} </div>;
-      })}
+      <input type="text" placeholder="Enter Some Text" />
     </div>
   );
 };
-
 export default InputElement;
